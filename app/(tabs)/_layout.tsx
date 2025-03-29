cae493c6b26f19813ceb43f71d2c23833e18376b
Hamza Abou Jaib <105951590+HamzaAbouJaib@users.noreply.github.com>
2025-03-29 18:58:40 -0400

    Something that half works

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,101 +1,127 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Image, Platform, TouchableOpacity } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, Platform, TouchableOpacity } from "react-native";
 
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 import { useRouter } from "expo-router";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import Login from "../login";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-  const router = useRouter(); 
+  const router = useRouter();
 
-  const color = useThemeColor({},'text');
+  const color = useThemeColor({}, "text");
 
-  return (
+  const login = <Login setIsLoggedIn={setIsLoggedIn} />;
+
+  const output = (
     <>
-      <ThemedView style={{ height: 120, padding: 0, alignItems: 'flex-end', flexDirection: 'row' }}>
+      <ThemedView
+        style={{
+          height: 120,
+          padding: 0,
+          alignItems: "flex-end",
+          flexDirection: "row",
+        }}
+      >
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
+            paddingBlock: 10,
+            paddingInline: 20,
+            alignItems: "center",
+          }}
+        >
+          <ThemedView style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image
+              source={require("@/assets/images/icon_silhouette.png")}
+              style={{ height: 40, width: 40, tintColor: color }}
+              resizeMode="contain"
+            />
 
-        <ThemedView style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%', paddingBlock: 10, paddingInline: 20, alignItems:'center' }}>
-         
-          <ThemedView style={{flexDirection:'row', alignItems:'center'}}>
-           <Image
-                    source={require('@/assets/images/icon_silhouette.png')}
-                    style={{height:40, width:40, tintColor:color}}
-                    resizeMode="contain"
-                  />
+            <ThemedText style={{ paddingLeft: 5 }} type="subtitle">
+              BirdScout
+            </ThemedText>
+          </ThemedView>
 
-            <ThemedText style={{paddingLeft:5}} type="subtitle">BirdScout</ThemedText>
-            </ThemedView>
-
-          <TouchableOpacity onPress={() => router.push("/account")} >
-           <Ionicons name="person-circle-sharp" size={32} color={color} />
+          <TouchableOpacity onPress={() => router.push("/account")}>
+            <Ionicons name="person-circle-sharp" size={32} color={color} />
           </TouchableOpacity>
-         
         </ThemedView>
-
       </ThemedView>
       <Tabs
         screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-          tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tabIconDefault,
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          tabBarInactiveTintColor:
+            Colors[colorScheme ?? "light"].tabIconDefault,
           headerShown: false,
           tabBarButton: HapticTab,
           tabBarBackground: TabBarBackground,
           tabBarStyle: Platform.select({
             ios: {
               // Use a transparent background on iOS to show the blur effect
-              position: 'absolute',
+              position: "absolute",
             },
             default: {},
           }),
-        }}>
-
+        }}
+      >
         <Tabs.Screen
           name="map"
           options={{
-            title: 'Map',
-            tabBarIcon: ({ color }) => <IconSymbol size={28} name="map.fill" color={color} />,
+            title: "Map",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="map.fill" color={color} />
+            ),
           }}
         />
 
         <Tabs.Screen
           name="index"
           options={{
-            title: 'Identify',
-            tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
+            title: "Identify",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="camera.fill" color={color} />
+            ),
           }}
         />
-
 
         <Tabs.Screen
           name="journal"
           options={{
-            title: 'Journal',
-            tabBarIcon: ({ color }) => <IconSymbol size={28} name="book.fill" color={color} />,
+            title: "Journal",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="book.fill" color={color} />
+            ),
           }}
         />
 
-<Tabs.Screen
+        <Tabs.Screen
           name="achievements"
           options={{
-            title: 'Achievements',
-            tabBarIcon: ({ color }) => <IconSymbol size={28} name="star.fill" color={color} />,
+            title: "Achievements",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="star.fill" color={color} />
+            ),
           }}
         />
-
       </Tabs>
     </>
   );
+
+  return isLoggedIn ? output : login;
 }
